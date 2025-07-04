fc8c701b8d5aab57063275b239dfad4a07a6b026
RaghibAbdien <frostline27@gmail.com>
2025-07-04 16:32:29 +0700

    fix: remove duplicate attributes from delete action button

--- resources/views/components/action-btn-datatables/guest-arrival-action.blade.php
+++ resources/views/components/action-btn-datatables/guest-arrival-action.blade.php
@@ -6,9 +6,7 @@
         @endif
         <x-action-button variant="gift" />
         <x-action-button variant="delete" aria-haspopup="dialog" data-hs-overlay="#delete-confirm-modal"
-            class="btn-delete" data-id="{{ $row->id }}" data-name="{{ $row->guest_name }}"aria-haspopup="dialog"
-            data-hs-overlay="#delete-confirm-modal" class="btn-delete" data-id="{{ $row->id }}"
-            data-name="{{ $row->guest_name }}" />
+            class="btn-delete" data-id="{{ $row->id }}" data-name="{{ $row->guest_name }}" />
         <button onclick="openCameraModal(this)" data-guest-name="{{ $row->guest_name }}"
             class="rounded bg-green-600 px-4 py-2 text-white hover:bg-green-700">
             Buka Kamera
